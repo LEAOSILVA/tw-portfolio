fad74eb5e654d333a547214542608a8479320a55
LEAOSILVA <natileao25@gmail.com>
2026-01-28 15:10:19 -0300

    reorganiza homepage

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -3,41 +3,65 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import styles from './styles.module.css';
 
-// Importando SVGs como React Components
-import PersonalInfoIcon from '@site/static/img/personal-info.svg';
-import FileSearchIcon from '@site/static/img/file-search.svg';
-import BlogIcon from '@site/static/img/blog.svg';
+const features = [
+  {
+    title: 'API Documentation',
+    link: '/docs/api',
+  },
+  {
+    title: 'FAQs',
+    link: '/docs/faqs',
+  },
+  {
+    title: 'Feature Specifications',
+    link: '/docs/feature-spec',
+  },
+  {
+    title: 'Glossary',
+    link: '/docs/glossary',
+  },
+  {
+    title: 'Release Notes',
+    link: '/docs/release-notes',
+  },
+  {
+    title: 'Scripts',
+    link: '/docs/scripts',
+  },
+  {
+    title: 'Tutorials',
+    link: '/docs/tutorials',
+  },
+  {
+    title: 'Use Cases',
+    link: '/docs/use-case',
+  },
+  {
+    title: 'UX Writing',
+    link: '/docs/ux-writing',
+  },
+];
 
 function HomepageFeatures() {
   return (
     <section className={styles.features}>
       <div className="container" style={{ padding: '2rem' }}>
-        <div className="row" style={{ justifyContent: 'center', gap: '2rem' }}>
-          
-          {/* Card 1: About Me */}
-          <div className={clsx('col col--5')}>
-            <div className={clsx(styles.card)}>
-              <Link to="/docs/about-me/who-am-i" className={styles.cardLink}>
-                <h3>About Me</h3>
-                <div className="text--center">
-                  <PersonalInfoIcon className={styles.featureSvg} role="img" />
-                </div>
-              </Link>
+        <div
+          className="row"
+          style={{
+            justifyContent: 'center',
+            gap: '1.5rem',
+          }}
+        >
+          {features.map(({ title, link }) => (
+            <div key={title} className={clsx('col col--4')}>
+              <div className={styles.card}>
+                <Link to={link} className={styles.cardLink}>
+                  <h3 className={styles.cardTitle}>{title}</h3>
+                </Link>
+              </div>
             </div>
-          </div>
-
-          {/* Card 2: Docs */}
-          <div className={clsx('col col--5')}>
-            <div className={clsx(styles.card)}>
-              <Link to="/docs/docs-example/api" className={styles.cardLink}>
-                <h3>Docs</h3>
-                <div className="text--center">
-                  <FileSearchIcon className={styles.featureSvg} role="img" />
-                </div>
-              </Link>
-            </div>
-          </div>
-
+          ))}
         </div>
       </div>
     </section>
@@ -45,5 +69,6 @@ function HomepageFeatures() {
 }
 
 export default HomepageFeatures;
+
 
 
